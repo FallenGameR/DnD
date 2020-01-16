0c2cba80440a111c1d570c121aa4c8e639a37f67
Alexander Kostikov <alexko@microsoft.com>
2020-01-15 18:36:42 -0800

    Trying out different approaches

--- DnDMusic/MainWindow.xaml.cs
+++ DnDMusic/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,21 @@ namespace DnDMusic
         public MainWindow()
         {
             InitializeComponent();
+            //this.cef.Address = @"https://www.youtube.com/watch?v=KZQBEynt3vA&list=PL2VAyAqd6lNqSm9sIi4u4ih7bzqSYNCDI&index=2&t=0s";
+
+            var start0 = new ProcessStartInfo(
+                @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe",
+                "--app=https://music.youtube.com");
+            var start = new ProcessStartInfo(
+                @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe",
+                @"https://www.youtube.com/embed/watch?autoplay=1&v=KZQBEynt3vA&list=PL2VAyAqd6lNqSm9sIi4u4ih7bzqSYNCDI&index=2&t=0s");
+            var process = new Process();
+            process.StartInfo = start;
+            process.Start();
+
+            //https://developers.google.com/youtube/player_parameters
+            //https://developers.google.com/identity/protocols/OAuth2InstalledApp
+            //Freemake
         }
     }
 }
